e7737043877a1cb279ecb53c0d7d928c4b18bc20
Junchen Liu <junchen.liu@gmail.com>
2015-12-25 19:16:36 +0000

    1st

---
+++ sping-first-project/src/main/java/uk/com/holychina/HelloController.java
@@ -0,0 +1,19 @@
+package uk.com.holychina;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		
+		ModelAndView welcomePage = new ModelAndView("Hello");
+		welcomePage.addObject("welcomeMsg", "Hi Junchen");
+		return welcomePage;
+	}
+ 
+}
---
+++ sping-first-project/src/main/webapp/WEB-INF/jsp/Hello.jsp
@@ -0,0 +1,8 @@
+
+
+<html>
+<body>
+	<h1>First Spring MVC Web App</h1>
+	<h2>${welcomeMsg}</h2>
+</body>
+</html>
