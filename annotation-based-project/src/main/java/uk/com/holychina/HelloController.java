74a5e72d379dd99260bc5b41d8501e6c3ddbf9f0
Junchen Liu <junchen.liu@gmail.com>
2015-12-25 20:52:12 +0000

    1st commit

---
+++ annotation-based-project/src/main/java/uk/com/holychina/HelloController.java
@@ -0,0 +1,17 @@
+package uk.com.holychina;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld(){
+		ModelAndView mv = new ModelAndView("Hello");
+		mv.addObject("welcomeMsg", "hello Junchen");
+		return mv;
+	}
+	
+}
